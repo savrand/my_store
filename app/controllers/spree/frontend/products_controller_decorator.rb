4672dbb1964535e5833e0f49c0d13297f8323574
dmitriy.savran <dmitriy.savran@faceit.com.ua>
2014-12-09 18:53:34 +0200

    test

--- app/controllers/spree/frontend/products_controller_decorator.rb
+++ app/controllers/spree/frontend/products_controller_decorator.rb
@@ -4,5 +4,6 @@ Spree::ProductsController.class_eval do
   def initialize_comment
     @comment = Spree::Comment.new
     @comments = @product.comments.preload(:user)
+    expire_fragment( [I18n.locale, current_currency, @product])
   end
 end
