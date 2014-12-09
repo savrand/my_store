77f3d0bb119d8564275d638d849fb1bacdc256f8
dmitriy.savran <dmitriy.savran@faceit.com.ua>
2014-12-09 19:32:02 +0200

    check

--- app/controllers/spree/comments_controller.rb
+++ app/controllers/spree/comments_controller.rb
@@ -2,8 +2,9 @@ class Spree::CommentsController < Spree::BaseController
   def create
     comment = spree_current_user.comments.build(comment_params)
     if comment.save
-      @product.touch
+      #@product.touch
       redirect_to :back
+      expire_fragment [I18n.locale, Spree::Config[:currency], @product, "spree/overrides/frontend/product_comments"]
     end
   end
 
