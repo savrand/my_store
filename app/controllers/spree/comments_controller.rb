99fb4fc15a4728cbf481e3ba04213bf04ff20bd5
dmitriy.savran <dmitriy.savran@faceit.com.ua>
2014-12-09 19:09:23 +0200

    test

--- app/controllers/spree/comments_controller.rb
+++ app/controllers/spree/comments_controller.rb
@@ -3,12 +3,13 @@ class Spree::CommentsController < Spree::BaseController
     comment = spree_current_user.comments.build(comment_params)
     if comment.save
       redirect_to :back
+      expire_fragment [I18n.locale, current_currency, @product]
     end
   end
 
   private
   def comment_params
-    product = Spree::Product.find_by_slug(params[:product_id])
-    params[:comment].merge(product_id:  product.id).permit(:product_id, :message)
+    @product = Spree::Product.find_by_slug(params[:product_id])
+    params[:comment].merge(product_id:  @product.id).permit(:product_id, :message)
   end
 end
--- app/controllers/spree/frontend/products_controller_decorator.rb
+++ app/controllers/spree/frontend/products_controller_decorator.rb
@@ -2,7 +2,6 @@ Spree::ProductsController.class_eval do
   before_action :initialize_comment, only: :show
   private
   def initialize_comment
-    expire_fragment( [I18n.locale, current_currency, @product])
     @comment = Spree::Comment.new
     @comments = @product.comments.preload(:user)
   end
