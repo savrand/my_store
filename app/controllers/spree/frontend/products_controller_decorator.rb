9e4e10575d7879ef83d04a3146efedf956eb47c6
dmitriy.savran <dmitriy.savran@faceit.com.ua>
2014-12-10 17:12:19 +0200

    add_some_commetns

--- app/controllers/spree/frontend/products_controller_decorator.rb
+++ app/controllers/spree/frontend/products_controller_decorator.rb
@@ -2,6 +2,7 @@ Spree::ProductsController.class_eval do
   before_action :initialize_comment, only: :show
   private
   def initialize_comment
+    #add comments to product show view
     @comment = Spree::Comment.new
     @comments = @product.comments.preload(:user)
   end
--- app/models/spree/comment.rb
+++ app/models/spree/comment.rb
@@ -1,6 +1,6 @@
 module Spree
   class Comment < Spree::Base
-    belongs_to :product, touch: true
+    belongs_to :product, touch: true #use touch for update cache on product show
     belongs_to :user
   end
 end
